df1085e1294242301943df6ccd33741883edc7db
PADimitrov23 <PADimitrov23@codingburgas.bg>
2026-01-03 22:19:25 +0200

    Add really old and unimportant changes

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -1,4 +1,17 @@
 extends Node
-
+#region Player Stats
 var gold = 0
 var health = 0
+#endregion
+
+#region Wave Logic Variables
+var waveCounter: int = 1
+var gobelinsSpawned: int = 0
+var gobelinsKilled: int = 0
+#endregion
+
+func startIntermission():
+	pass
+
+func startWave():
+	pass
