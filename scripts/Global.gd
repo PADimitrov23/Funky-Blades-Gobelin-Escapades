cd5aa865a66b379ddcb10e85ba4eba50d35f7fd1
Peter Dimitrov <PADimitrov23@codingburgas.bg>
2026-01-20 22:47:58 +0200

    Start working on base wave logic

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -6,9 +6,6 @@ static var health = 100
 
 #region Wave Logic Variables
 static var waveCounter: int = 1
-static var gobelinsPerWave: Curve = load("res://data/gobelins_per_wave.tres")
-static var gobelinsSpawned: int = 0
-static var gobelinsKilled: int = 0
 static var inWave: bool = false
 #endregion
 
@@ -16,4 +13,7 @@ func startIntermission():
 	$tavern/TavernDoorWorkability.unlock()
 
 func startWave():
-	pass
+	$tavern/TavernDoorWorkability.lock()
+	#THE SKY CHANGES AND THERES A TEXT MESSAGE
+	 
+	
--- scripts/respawner.gd
+++ scripts/respawner.gd
@@ -2,6 +2,15 @@ extends Node3D
 
 @export var enemy : PackedScene
 
+static var gobelinsPerWave: Curve = load("res://data/gobelins_per_wave.tres")
+static var gobelinsSpawned: int = 0
+static var gobelinsMax
+
+func spawnGobelins(waveCounter):
+	gobelinsMax = gobelinsPerWave.sample(waveCounter)
+	#insert the weighted picker
+	
+
 
 func _on_timer_timeout() -> void:
 	var instance = enemy.instantiate()
